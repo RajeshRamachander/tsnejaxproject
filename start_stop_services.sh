e10289461b81bd458885ae2d9029564bb00d9751
rrajesh27 <ramachander.rajesh@gmail.com>
2024-02-03 10:51:03 +0530

    Updated

--- start_stop_services.sh
+++ start_stop_services.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 if [ "$1" == "start" ]; then
-    # Start RabbitMQ in detached mode
-    echo "Starting RabbitMQ..."
-    /usr/local/opt/rabbitmq/sbin/rabbitmq-server -detached
-
-    # Wait for RabbitMQ to start
-    while ! nc -z localhost 5672; do
-      sleep 1
-    done
-
-    echo "RabbitMQ has started."
-
     # Start Redis in detached mode
     echo "Starting Redis..."
     redis-server --daemonize yes
@@ -26,8 +15,6 @@ if [ "$1" == "start" ]; then
     # Start Celery Worker (Replace 'your_app' with your actual app name) in detached mode
     echo "Starting Celery Worker..."
     celery -A server.celery worker --loglevel=info --detach &
-
-    # Optionally, start your Flask or Django application here if needed
 
     # End of script
     echo "All services started successfully."
@@ -56,17 +43,6 @@ elif [ "$1" == "stop" ]; then
     done
 
     echo "Redis stopped."
-
-    # Stop RabbitMQ
-    echo "Stopping RabbitMQ..."
-    /usr/local/opt/rabbitmq/sbin/rabbitmqctl stop
-
-    # Wait for RabbitMQ to stop
-    while pgrep -f 'rabbitmq-server' > /dev/null; do
-      sleep 1
-    done
-
-    echo "RabbitMQ stopped."
 
     echo "All services stopped successfully."
 
