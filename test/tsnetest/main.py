fc1c2bfec76814a8494ee1b5af316abe6ae00003
RajeshRamachander <ramachander.rajesh@gmail.com>
2024-03-14 20:47:17 +0530

    updated

--- test/tsnetest/main.py
+++ test/tsnetest/main.py
@@ -1,33 +1,3 @@
-# import time
-# from server_communicator import ServerCommunicator
-# from simple_data_processor import SimpleDataProcessor
-
-# if __name__ == '__main__':
-#     server_communicator = ServerCommunicator()
-#     algorithms = ['ntk', 'jax_tsne', 'sklearn_tsne']
-
-#     for algorithm in algorithms[::-1]:
-#         print(f"Starting t-SNE with {algorithm} algorithm.")
-#         data_processor = SimpleDataProcessor(algorithm)
-
-#         main_data = data_processor.prepare_data()
-#         response = server_communicator.start_task(main_data)
-
-#         if response.status_code == 202:
-#             task_id = response.json()['task_id']
-#             print(f'Task started with ID: {task_id}')
-
-#             start_time = time.time()
-#             processed_result = data_processor.wait_for_completion(task_id, server_communicator)
-#             end_time = time.time()
-#             execution_time = end_time - start_time
-#             print(f"Total execution time: {execution_time} seconds")
-
-#             data_processor.output_data_processor(processed_result)
-#         else:
-#             print(f'Error starting the task: {response.status_code}')
-#         print("\n")
-
 import time
 import asyncio
 import nest_asyncio
